bf5a8b20029827ae5c58c932f45e85673ccc0f97
Nemanja Tesic <nemanjatesic96@gmail.com>
2025-08-16 21:32:29 +0200

    update

--- _next/static/chunks/app/page-44921ccb9b0eb6fd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7410:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8909,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[909,130,427,744],()=>s(7410)),_N_E=e.O()}]);
