ccd604e983908c6d79b324b6ec84ae626307460d
Nemanja Tesic <nemanjatesic96@gmail.com>
2026-07-06 00:21:22 +0200

    update

---
+++ assets/esm-B-YJt-_J.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BrBQ-yvn.js","assets/index-CaLpS7vk.js","assets/index-y5MoFDdR.css"])))=>i.map(i=>d[i]);
+import{a as e,o as t}from"./index-CaLpS7vk.js";var n=e(`Browser`,{web:()=>t(()=>import(`./web-BrBQ-yvn.js`),__vite__mapDeps([0,1,2])).then(e=>new e.BrowserWeb)});export{n as Browser};
